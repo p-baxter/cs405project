1caf07f3c02c3d090fb0bb384c9c92a89498d1e6
mdenninghoff <mdenninghoff@users.noreply.github.com>
2015-04-12 00:20:18 -0400

    Create constants.php

---
+++ admin/includes/constants.php
@@ -0,0 +1,51 @@
+<?php
+/* 
+ * constants.php
+ * 
+ * Define global constants for use in the application here.
+ * 
+ * The MIT License
+ *
+ * Copyright 2015 matt.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+define('SESSION_ID_KEY', 'sessID');
+
+define('STORE_NAME', 'Happy\'s Fun time Game and Toy Store');
+define('STORE_LOGO_IMG', 'logo.jpg');
+
+define('SITE_BASE_URL', 'http://localhost/cs405/');
+
+define('DIR_IMAGES', 'images/');
+define('DIR_IMAGES_PRODUCTS', 'images/products/');
+
+define('FILE_LOGIN', 'login.php');
+define('FILE_ITEMS', 'items.php');
+define('FILE_ORDERS', 'orders.php');
+define('FILE_REPORTS', 'reports.php');
+define('FILE_SPECIALS', 'specials.php');
+
+define('MYSQLI_BIND_TYPE_INT',    'i');
+define('MYSQLI_BIND_TYPE_DOUBLE', 'd');
+define('MYSQLI_BIND_TYPE_STRING', 's');
+define('MYSQLI_BIND_TYPE_BLOB',   'b');
+
+define('TIMEZONE_DEFAULT', 'America/New_York');
