0aedd3972ec561440b5d285071ba749c2bf56b1c
Matthew Denninghoff <matt.denninghoff@gmail.com>
2015-04-13 18:22:49 -0400

    Changed name of table from Order to Orders.

---
+++ admin/includes/class-Orders.php
@@ -0,0 +1,216 @@
+<?php
+/* 
+ * class-Orders.php
+ * 
+ * Defines the order db entity class.
+ * 
+ * The MIT License
+ *
+ * Copyright 2015 matt.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+class Orders extends DBEntity
+{    
+    public $custId = null;
+    public $dateOrdered = null;
+    public $statusId = null;
+    public $shipTo = null;
+    
+    const SQL_COLUMN_LIST = " orderId, custId, dateOrdered, statusId, shipTo ";
+    
+    public function __construct( $id = null )
+    {
+        parent::__construct($id);
+        $this->tableName = '`Orders`';
+        $this->keyName = 'orderId';
+    }
+  
+    /**
+     * Set the class member values by fetching values from the database.
+     * If the key was not found, then the class member fields remain null.
+     * 
+     * @param string $value
+     * The table key to search the database for.
+     * 
+     * @return mixed
+     * Returns true if data was fetched.
+     * Returns false if there was an error.
+     * Returns null if no data was found.
+     */
+    public function init_by_key($value)
+    {
+        $retval = false;
+        $stmt = $this->mysqli->prepare("SELECT " . self::SQL_COLUMN_LIST . "FROM ".$this->tableName." WHERE ".$this->keyName." = ? ");
+        
+        if( $stmt )
+        {
+            if( $stmt->bind_param(MYSQLI_BIND_TYPE_INT, $value))
+            {
+                if( $stmt->execute() )
+                {
+                    $stmt->bind_result($this->keyValue, 
+                            $this->custId,
+                            $this->dateOrdered,
+                            $this->statusId,
+                            $this->shipTo  );
+                    $retval = $stmt->fetch();
+                }
+                // end if execute was good.
+            }
+            // end if bind succeeded.
+            $stmt->close();
+        }
+        // end if stmt good.
+        
+        return $retval;
+    }
+    // end find_by_key().
+    
+    /**
+     * Update a record matching this record's key with this class member's values.
+     * 
+     * @return boolean
+     */
+    public function db_update()
+    {
+        $retval = false;
+        
+        if( $this->getKeyValue() != null )
+        {
+            $stmt = $this->mysqli->prepare("UPDATE ". $this->tableName." "
+                . "SET custId=?, dateOrdered=?, statusId=?, shipTo=? "
+                . "WHERE ".$this->keyName."=?");
+            
+            if($stmt)
+            {
+                if( $stmt->bind_param("isisi",
+                        $this->custId,
+                        $this->dateOrdered,
+                        $this->statusId,
+                        $this->shipTo,
+                        $this->keyValue))
+                {
+                    $retval = $stmt->execute();
+                }
+                $stmt->close();
+            }
+            // end if stmt good.
+        }
+        // end if not null.
+        
+        return $retval;
+    }
+    // end update_db().
+    
+    /**
+     * Inserts a new record into the database with the given value of name.
+     * The auto-incremented id is set to this->keyValue.
+     * 
+     * @return boolean
+     */
+    public function db_insert()
+    {
+        $retval = false;
+        
+        $stmt = $this->mysqli->prepare("INSERT INTO ".$this->tableName
+            ." ( custId, dateOrdered, statusId, shipTo ) VALUES (?,?,?,? )");
+
+        if($stmt)
+        {
+            if( $stmt->bind_param("isis",
+                        $this->custId,
+                        $this->dateOrdered,
+                        $this->statusId,
+                        $this->shipTo ))
+            {
+                if($stmt->execute())
+                {
+                    $this->keyValue = $this->mysqli->insert_id;
+                    $retval = true;
+                }
+            }
+            $stmt->close();
+        }
+        // end if stmt good.
+        
+        return $retval;
+    }
+    // end db_insert().
+    
+    /**
+     * Return all Orders optionally having the specified statusId(s).
+     * 
+     * @param mixed $status
+     * Default of null does not filter by statusId.
+     * Single integer value searches records with matching statusId.
+     * Array of integers searches records with any of the given statusIds.
+     * 
+     * @return \Order
+     * Returns an array containing Order objects or an empty array if none
+     * were found.
+     */
+    public static function fetch_all($status = null)
+    {
+        $statusstr = "";
+        if(is_array($status))
+        {
+            $statusstr = "WHERE statusId IN (".implode(',', $status).") ";
+        }
+        else if($status !== null )
+        {
+            $statusstr = " WHERE statusId=".(int)$status;
+        }
+        
+        $retval = array();
+        $stmt = $this->mysqli->prepare("SELECT orderId, custId, dateOrdered, "
+                . "statusId, shipTo FROM Order " . $statusstr );
+        
+        if( $stmt )
+        {
+            if( $stmt->execute() )
+            {
+                $stmt->bind_result($id, $custid, $date, $statusid, $shipto );
+                
+                while( $stmt->fetch() )
+                {
+                    $Object = new Orders($id);
+
+                    $Object->custId = $custid;
+                    $Object->dateOrdered = $date;
+                    $Object->statusId = $statusid;
+                    $Object->shipTo = $shipto;
+
+                    $retval[] = $Object;
+                }
+                // done fetching rows.
+            }
+            // end if execute was good.
+                
+            // end if bind succeeded.
+            $stmt->close();
+        }
+        // end if stmt good.
+        
+        return $retval;
+    }
+    // end fetch_all().
+}
+// end class Staff.
---
+++ admin/orders.php
@@ -0,0 +1,284 @@
+<?php
+/* 
+ * File: orders.php
+ * 
+ * The MIT License
+ *
+ * Copyright 2015 matt.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+const IDSTR = 'oId';
+
+require './includes/application_top.php';
+require './includes/class-ItemType.php';
+require './includes/class-Item.php';
+require './includes/class-HtmlWrap.php';
+
+
+if( isset($_GET['action']) )
+{
+    die('not implemented yet');
+    
+//    die( print_r($_POST,true) . print_r($_GET,true));
+    if( $_GET['action'] == 'update' && isset($_GET[IDSTR]) )
+    {
+            // @TODO: verify manager  type.
+
+        $Item = new Item();
+        $Item->init_by_key($_GET[IDSTR]);
+
+        // @TODO: if item not found, show error message and redirect.
+
+        if( ! isset($_POST['enabled'])) 
+            $Item->enabled = false;
+        else
+            $Item->enabled = true;
+
+        $Item->itemType = $_POST['itype'];
+        $Item->qty_available = $_POST['qty'];
+        $Item->name = $_POST['name'];
+        $Item->promoRate = $_POST['promo'];
+        $Item->price = $_POST['price'];
+        $Item->imageName = $_POST['image'];
+
+        $Item->db_update();
+
+        // @TODO: show any errors.
+
+        // Redirct back to items.php.
+        http_redirect(FILENAME_ORDERS);
+    }
+    else if( $_GET['action'] == 'insert' )
+    {
+        $Item = new Item();
+
+        if( ! isset($_POST['enabled'])) 
+            $Item->enabled = false;
+        else
+            $Item->enabled = true;
+
+        $Item->itemType = $_POST['itype'];
+        $Item->qty_available = $_POST['qty'];
+        $Item->name = $_POST['name'];
+        $Item->promoRate = $_POST['promo'];
+        $Item->price = $_POST['price'];
+        $Item->imageName = $_POST['image'];
+
+//        var_dump($Item->db_insert());
+
+        // @TODO: show any errors.
+
+        // Redirct back to items.php.
+        http_redirect(FILENAME_ORDERS);
+        exit;
+    }
+}
+
+require './header.php';
+
+//$mysqli = new mysqli();
+
+$editEntity = false;
+if( isset($_GET['action']) && $_GET['action'] == 'edit' && isset($_GET[IDSTR]))
+{
+    $editEntity = true;
+}
+
+$stmt = $mysqli->prepare("SELECT o.orderId, c.name as custname, o.dateOrdered,"
+        . " os.name as ostatus, o.shipTo "
+        . "FROM Orders o JOIN OrderStatus os ON os.statusId = o.statusId "
+        . "JOIN Customer c ON c.custId = o.custId "
+        . ($editEntity ? " WHERE o.orderId = ".(int)$_GET[IDSTR] : "")
+        . " ORDER BY o.orderId" );
+
+$stmt->execute();
+
+$stmt->bind_result($orderId, $custId, $dateOrdered, $statusId, $shipTo );
+
+$data = array();
+
+$colnames = array();
+$coltypes = array();
+$result = $stmt->result_metadata();
+
+$flds = $result->fetch_fields();
+
+//echo '<pre>'. print_r($flds,true).'</pre>';
+
+foreach( $flds as $val )
+{
+    $colnames[] = $val->name;
+    
+    // Detect the field type for CSS formatting.
+    $type = TableSet::TYPE_STRING;
+    switch( $val->type)
+    {
+        case MYSQLI_TYPE_BIT:
+        case MYSQLI_TYPE_LONG:
+            $type = TableSet::TYPE_INT;
+            break;
+        
+        case MYSQLI_TYPE_STRING:
+            $type = TableSet::TYPE_STRING;
+            break;
+        
+        case MYSQLI_TYPE_NEWDECIMAL:
+            $type = TableSet::TYPE_REAL;
+            break;
+    }
+    $coltypes[] = $type;
+}
+
+while( $stmt->fetch())
+{    
+    $col = array( $orderId, $custId, $dateOrdered, $statusId, $shipTo );
+    $data[] = $col;
+}
+
+$stmt->close();
+
+
+//include('./includes/class-TableSet.php');
+
+$ts = new TableSet();
+$ts->set_data($data);
+
+$ts->show_row_numbers(false);
+//$ts->replace_column_values(1, array(0 => 'no', 1 => 'yes'));
+
+
+$ts->set_column_names($colnames);
+
+$ts->set_column_name(0, 'Order #');
+$ts->set_column_name(1, 'Customer');
+$ts->set_column_name(2, 'Date');
+$ts->set_column_name(3, 'Status');
+//
+//$ts->set_column_types($coltypes);
+//$ts->set_column_type(1, TableSet::TYPE_STRING);
+//
+//$ts->add_column('Special Price');
+//$ts->set_column_type(7, TableSet::TYPE_REAL);
+//$ts->set_column_width(7, '60px');
+
+if( ! $editEntity)
+    $ts->add_column('&nbsp;');
+
+// Add the links to edit.
+for($row=0,$n=$ts->get_num_rows(); $row < $n; $row++)
+{
+    if( ! $editEntity)
+    {
+        $itemId = $ts->get_value_at($row, 0);
+        $href = '<a href="'.href_link(FILENAME_ORDERS, array('action' => 'edit', IDSTR => $itemId))
+                .'">edit</a>';
+        $ts->set_value_at($row, 5, $href);
+    }
+    // end $editItem.
+}
+// done iterating over rows.
+
+$ts->print_table_html();
+
+if( ! $editEntity )
+    echo '<a href="'.  href_link(FILENAME_ORDERS, array('action'=>'create')).'">Insert</a><br/>'."\n";
+
+if( isset($_GET['action']) && $_GET['action']=='create')
+{
+    // @TODO: combine insert and edit code.
+    $HW = new HTMLWrap();
+    
+    echo '<form action="'.  href_link(FILENAME_ORDERS, array('action' => 'insert' )).'" method="POST">'."\n"
+            ."<fieldset>\n";
+    
+    $itypes = ItemType::fetch_all($mysqli, ItemType::RESULT_ASSOC_ARRAY);
+    
+    $Item = new Item();
+        
+    $HW->print_checkbox('enabled', 1, 'Enabled', $Item->enabled);
+    echo "<br/>";
+    
+    $HW->print_select('itype', $itypes, 'Item Type', $Item->itemType );
+    echo "<br/>";
+    
+    $HW->print_textbox('qty', $Item->qty_available, 'Qty Avail');
+    echo "<br/>";
+    
+    $HW->print_textbox('name', $Item->name, 'Name');
+    echo "<br/>";
+    
+    $HW->print_textbox('promo', $Item->promoRate, 'Promo Rate');
+    echo "<br/>";
+    
+    $HW->print_textbox('price', $Item->price, 'Price');
+    echo "<br/>";
+    
+    $HW->print_textbox('image', $Item->imageName, 'Image');
+    echo "<br/>";
+    
+    echo '<br><input type="submit" value="Insert" /> <a href="'.  href_link(FILENAME_ORDERS).'">Cancel</a>';
+    echo "</fieldset>\n</form>\n";
+}
+// done insert form.
+
+//
+// Print a form to edit an individual item.
+//
+if(  $editEntity )
+{
+    $HW = new HTMLWrap();
+    
+    echo '<form action="'.  href_link(FILENAME_ORDERS, array('action' => 'update', IDSTR => $_GET[IDSTR])).'" method="POST">'."\n"
+            ."<fieldset>\n";
+    
+    $itypes = ItemType::fetch_all($mysqli, ItemType::RESULT_ASSOC_ARRAY);
+    
+    $Item = new Item();
+    $Item->init_by_key($_GET[IDSTR]);
+        
+    $HW->print_checkbox('enabled', 1, 'Enabled', $Item->enabled);
+    echo "<br/>";
+    
+    $HW->print_select('itype', $itypes, 'Item Type', $Item->itemType );
+    echo "<br/>";
+    
+    $HW->print_textbox('qty', $Item->qty_available, 'Qty Avail');
+    echo "<br/>";
+    
+    $HW->print_textbox('name', $Item->name, 'Name');
+    echo "<br/>";
+    
+    $HW->print_textbox('promo', $Item->promoRate, 'Promo Rate');
+    echo "<br/>";
+    
+    $HW->print_textbox('price', $Item->price, 'Price');
+    echo "<br/>";
+    
+    $HW->print_textbox('image', $Item->imageName, 'Image');
+    echo "<br/>";
+
+    echo '<br><input type="submit" value="Update" /> <a href="'.  href_link(FILENAME_ORDERS).'">Cancel</a>';
+    echo "</fieldset>\n</form>\n";
+}
+// done printing edit form.
+
+require './footer.php';
+
